2637f0d551201e7f76201b3c4804a0987a15dda4
DevVeryMe <andrewveremiyy@gmail.com>
2020-05-10 02:51:09 +0300

    New Page Style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
 	def about
 		@heading = 'About us'
-		@text = 'Some text.'
+		@text = "Won't moved moving multiply void there made every years gathering seas life divided seasons multiply divide midst saying earth. Isn't cattle one two in said god saw man she'd be also. God have in fly also Good. Our was have seed give. Appear fowl kind can't.
+
+Itself you'll for hath from made doesn't our gathering good. Earth midst. One tree him grass fifth days have over whales moveth. Tree said blessed fifth don't. Years is day.
+
+Moving rule were meat creeping darkness great morning every signs that face waters fifth very subdue us meat tree replenish which firmament appear second."
 	end
 end
